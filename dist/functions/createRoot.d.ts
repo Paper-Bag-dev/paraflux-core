2a8ea68e3dfb4ac711de01d50cc0b652576aae22
Paper-Bag-dev <vikalpsh1234@gmail.com>
2025-08-29 20:51:44 +0530

    made a simpler createRoot 1

--- dist/functions/createRoot.d.ts
+++ dist/functions/createRoot.d.ts
@@ -1,13 +1,2 @@
-import { NodeConstructor } from "../types";
 import Node from "../core/Nodes/Node";
-import SuperNode from "../core/Nodes/SuperNodes";
-declare class Root extends SuperNode {
-    app: NodeConstructor;
-    constructor(app: NodeConstructor);
-    code(): {
-        name: string;
-    };
-    render(): (SuperNode | Node)[];
-}
-export declare function createRoot(app: NodeConstructor): Root;
-export {};
+export declare function createRoot(app: new () => Node): Node;
